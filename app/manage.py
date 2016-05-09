a25e7a37df9ca0dc813c39099a3e9c5ec4cc91bd
md.khallaghi <md.khallaghi@gmail.com>
2016-05-09 02:07:51 -0400

    s

--- app/manage.py
+++ app/manage.py
@@ -4,6 +4,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask.ext.script import Manager, Server
 from app import app
 
-app.debug = True
+app['debug'] = True
 if __name__ == "__main__":
     app.run()
