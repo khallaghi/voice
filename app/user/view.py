1780b8284b434a5f3cd4214e8b31230d694aa7db
Mohammad Khallaghi <md.khallaghi@gmail.com>
2016-07-21 03:58:17 +0430

    adding add fac and add uni page

--- app/user/view.py
+++ app/user/view.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request, redirect, render_template, url_for
 from flask.views import MethodView
 from app.models import University, Professor, Faculty, Post
 from flask.ext.mongoengine.wtf import model_form
-from app.forms import ProfForFacForm, ProfForm
+from app.forms import ProfForFacForm, ProfForm, FacultyForm, UniversityForm
 from mongoengine import Q
 from flask import jsonify
 from app.auth import requires_auth
@@ -79,7 +79,51 @@ class UserAddProfessor(MethodView):
 
 		return render_template('user/add-prof-user.html', profs = profs, form = form, msg="fail" )
 	
-user.add_url_rule('/user/add/prof', view_func = UserAddProfessor.as_view('user_add_prof'))
+user.add_url_rule('/user/add/professor', view_func = UserAddProfessor.as_view('user_add_prof'))
+
+class UserAddFaculty(MethodView):
+	def get_form(self):
+		form = FacultyForm(request.form)
+		form.uni.choices = [(str(u.id), u.name)for u in University.objects.all()]
+		return form
+	def get(self):
+		form = self.get_form()		
+		return render_template('user/add-fac-user.html',
+								 form=form,
+								 msg=request.args.get('msg'))
+	def post(self):
+		form = self.get_form()
+		try:
+			if form.validate():
+				uni = University.objects(id = form.uni.data).first()
+				if Faculty.objects(uni = uni, name__contains=form.name.data).first() != None:
+					return redirect(url_for("user.user_add_faculty", msg="repeatitive"))
+
+				fac = Faculty()
+				fac.name = form.name.data
+				fac.uni = University.objects(id = form.uni.data).first()
+				fac.published = False
+				fac.save()
+				return redirect(url_for("user.user_add_faculty", msg="successful"))
+			return render_template("user/add-fac-user.html", form=form, msg="fail")
+		except:
+			return redirect(url_for("user.user_add_faculty", msg=fail))
+user.add_url_rule("/user/add/faculty", view_func=UserAddFaculty.as_view("user_add_faculty"))
+
+class UserAddUni(MethodView):
+	def get(self):
+		form = UniversityForm(request.form)
+		msg = request.args.get("msg")
+		return render_template('user/add-uni-user.html', form=form, msg=msg)
+	def post(self):
+		form = UniversityForm(request.form)
+		if form.validate():
+			if University.objects(name__contains = form.name.data).first() != None:
+				return redirect(url_for("user.user_add_uni", msg="repeatitive"))
+			University(name = form.name.data, published = False).save()
+			return redirect(url_for("user.user_add_uni", msg="successful"))
+		return render_template("user/add-uni-user.html", form=form, msg="fail")
+user.add_url_rule("/user/add/uni", view_func=UserAddUni.as_view("user_add_uni"))
 
 class UserAddProfToFac(MethodView):
 	def post(self, fac_id):
